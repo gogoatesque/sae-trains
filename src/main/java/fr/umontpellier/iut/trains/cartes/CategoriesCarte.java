32507c948b5c51ffd9cad7f1aa09785f1cb3970e
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-03-29 09:47:26 +0100

    Resoudre bug

--- src/main/java/fr/umontpellier/iut/trains/cartes/CategoriesCarte.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CategoriesCarte.java
@@ -1,22 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class CategoriesCarte {
+public enum CategoriesCarte {
     /**
      * Enumération des catégories des cartes
      */
-    public enum CouleurJoueur {
+
         OR, ROUGE, VERTE, BLEU, VIOLET, GRIS;
 
         @Override
         public String toString() {
-            return switch (this) {
-                case OR -> "or";
-                case ROUGE -> "rouge";
-                case VERTE -> "verte";
-                case BLEU -> "bleu";
-                case VIOLET -> "violet";
-                case GRIS -> "gris";
-            };
+            return super.toString().toLowerCase();
         }
-    }
 }
