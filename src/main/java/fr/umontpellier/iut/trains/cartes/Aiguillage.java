b9af02ea4786dafcdd1b5da057740f3a0e812ca9
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-03-29 11:05:13 +0100

    carte Aiguillage

--- src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Aiguillage extends Carte {
     public Aiguillage() {
-        super("Aiguillage");
+        super("Aiguillage", 5);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        joueur.piocher(2);
     }
 }
