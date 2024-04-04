7f8fe42445e7ec1ba2d6886695550416f65b4b6d
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 10:04:18 +0200

    création de TGV

--- src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
@@ -1,7 +1,19 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class TGV extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class TGV extends CarteBleue {
     public TGV() {
-        super("TGV");
+        super("TGV",2,1);
+        addCategorie(CategoriesCarte.ROUGE);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        for (Carte carte :joueur.getCartesEnJeu()){
+            if (carte.getNom().equals("Train omnibus")){
+                joueur.addArgent(1);
+            }
+        }
     }
 }
