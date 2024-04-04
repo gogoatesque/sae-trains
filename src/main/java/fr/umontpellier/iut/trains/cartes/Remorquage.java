6b08d1475ecad3332fd754ce9541081a0c29c641
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 09:57:23 +0200

    création de Remorquage

--- src/main/java/fr/umontpellier/iut/trains/cartes/Remorquage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Remorquage.java
@@ -1,7 +1,24 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class Remorquage extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Remorquage extends CarteRouge {
     public Remorquage() {
-        super("Remorquage");
+        super("Remorquage",3,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> choix = new ArrayList<>();
+        for (Carte carte : joueur.getDefausse()) {
+            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) {
+                choix.add(carte.getNom());
+            }
+        }
+        Carte train = joueur.getCartesEnJeu().getCarte(joueur.choisir("Choisissez une cartes Train de votre défausse", choix, null, false));
+        joueur.prendreCarteDefausse(train);
     }
 }
