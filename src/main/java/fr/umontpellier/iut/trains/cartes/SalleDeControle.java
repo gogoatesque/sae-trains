7c1bff485b0df248387019f4854e656991c5a8c3
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 10:02:19 +0200

    création de SalleDeControle

--- src/main/java/fr/umontpellier/iut/trains/cartes/SalleDeControle.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/SalleDeControle.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class SalleDeControle extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class SalleDeControle extends CarteRouge {
     public SalleDeControle() {
-        super("Salle de contrôle");
+        super("Salle de contrôle",3,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        joueur.piocher(3);
     }
 }
