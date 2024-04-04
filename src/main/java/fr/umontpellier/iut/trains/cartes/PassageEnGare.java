99a749818ba8afb4832d4ca20061cc84ef197200
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 09:45:45 +0200

    création de PassageEnGare

--- src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PassageEnGare.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class PassageEnGare extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class PassageEnGare extends CarteRouge {
     public PassageEnGare() {
-        super("Passage en gare");
+        super("Passage en gare",3,1);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        super.jouerCarte(joueur);
+        joueur.piocher();
     }
 }
