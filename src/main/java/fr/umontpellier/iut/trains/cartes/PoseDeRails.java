52fd721152960a910532b5538441222025789e85
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 09:49:32 +0200

    création de PoseDeRails

--- src/main/java/fr/umontpellier/iut/trains/cartes/PoseDeRails.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PoseDeRails.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class PoseDeRails extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class PoseDeRails extends CarteVerte {
     public PoseDeRails() {
-        super("Pose de rails");
+        super("Pose de rails",3);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+
     }
 }
