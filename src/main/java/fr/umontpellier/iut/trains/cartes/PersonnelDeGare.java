ea125b1dc70da74a689c4df0d95a39697b36bdc2
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 09:45:28 +0200

    création de PersonnelDeGare

--- src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
@@ -1,7 +1,34 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class PersonnelDeGare extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class PersonnelDeGare extends CarteRouge {
     public PersonnelDeGare() {
-        super("Personnel de gare");
+        super("Personnel de gare",2,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> choix = new ArrayList<>(Arrays.asList("piocher", "argent", "ferraille"));
+        String action = joueur.choisir("Choisissez entre 'piocher', 'argent' et 'ferraille'", choix, null, false);
+        switch (action) {
+            case "piocher" -> joueur.piocher();
+            case "argent" -> joueur.addArgent(1);
+            case "ferraille" -> {
+                for (Carte carte : joueur.getMain()) {
+                    if (carte.getCategorie().contains(CategoriesCarte.GRIS)) {
+                        joueur.getJeu().ajouterDansLaReserve(carte);
+                        joueur.enleverCarteMain(carte);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
