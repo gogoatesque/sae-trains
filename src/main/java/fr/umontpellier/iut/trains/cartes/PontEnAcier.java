4f22c0ad474488bc8657c506c25ae292ab3a6d98
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 09:47:58 +0200

    création de PontEnAcier (sans l'effet passif)

--- src/main/java/fr/umontpellier/iut/trains/cartes/PontEnAcier.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PontEnAcier.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class PontEnAcier extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class PontEnAcier extends CarteVerte {
     public PontEnAcier() {
-        super("Pont en acier");
+        super("Pont en acier",4);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        //effet permanent
     }
 }
