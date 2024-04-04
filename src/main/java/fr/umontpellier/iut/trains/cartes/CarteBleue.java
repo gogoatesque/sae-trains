70ce55dc25c4bb46a0205c0e3590cb0b422cb2db
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 08:37:49 +0200

    ajout getteur valeur

--- src/main/java/fr/umontpellier/iut/trains/cartes/CarteBleue.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CarteBleue.java
@@ -4,15 +4,20 @@ import fr.umontpellier.iut.trains.Joueur;
 
 public abstract class CarteBleue extends Carte {
 
-    private int valeur;
+    private final int valeur;
 
-    public CarteBleue(String nom, int valeur, int cout) {
+    public CarteBleue(String nom, int cout, int valeur) {
         super(nom, cout);
         this.valeur = valeur;
+        addCategorie(CategoriesCarte.BLEU);
     }
 
     @Override
     public void faireAction(Joueur joueur) {
         joueur.addArgent(valeur);
+    }
+
+    public int getValeur() {
+        return valeur;
     }
 }
