d7070243bcde6e0f941ed4abdfdfdb1cbea3457f
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-04 08:39:47 +0200

    création de ParcDAttractions

--- src/main/java/fr/umontpellier/iut/trains/cartes/ParcDAttractions.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/ParcDAttractions.java
@@ -1,7 +1,26 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class ParcDAttractions extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParcDAttractions extends CarteRouge {
     public ParcDAttractions() {
-        super("Parc d'attractions");
+        super("Parc d'attractions",4,1);
+
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        super.faireAction(joueur);
+        List<String> choix = new ArrayList<>();
+        for (Carte carte : joueur.getCartesEnJeu()) {
+            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) {
+                choix.add(carte.getNom());
+            }
+        }
+        Carte train = joueur.getCartesEnJeu().getCarte(joueur.choisir("Choisissez une de vos cartes Train", choix, null, false));
+        joueur.addArgent(train.getValeur());
     }
 }
