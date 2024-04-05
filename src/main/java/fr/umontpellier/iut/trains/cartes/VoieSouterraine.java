96849d185d597bcc7f07e11dfc64a0868b331703
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-05 11:22:29 +0200

    création VoieSouterraine

--- src/main/java/fr/umontpellier/iut/trains/cartes/VoieSouterraine.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/VoieSouterraine.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class VoieSouterraine extends Carte {
     public VoieSouterraine() {
-        super("Voie souterraine");
+        super("Voie souterraine",7);
+    }
+
+    @Override
+    public void jouerCarte(Joueur Joueur) {
+        //effet passif
     }
 }
