dd57db1e12a61d3bbc20918036c49ba53bb387c8
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-05 11:22:04 +0200

    création wagon incomplete

--- src/main/java/fr/umontpellier/iut/trains/cartes/UsineDeWagons.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/UsineDeWagons.java
@@ -1,7 +1,25 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class UsineDeWagons extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsineDeWagons extends CarteRouge {
     public UsineDeWagons() {
-        super("Usine de wagons");
+        super("Usine de wagons",5,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> choix = new ArrayList<>();
+        for (Carte carte : joueur.getMain()) {
+            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) choix.add(carte.getNom());
+        }
+        Carte train = joueur.getMain().getCarte(joueur.choisir("Choisissez une carte Train de votre main a écartée", choix, null, false));
+        if(train != null) {
+            joueur.ecarteCarte(train);
+            choix = joueur.getJeu().getReserve().keySet().stream().filter(nomCarte -> joueur.getJeu().getReserve().get(nomCarte).getCarte(nomCarte).getCout() <= train.getValeur()+3).map(nomCarte -> "ACHAT:"+nomCarte).toList();
+        }
     }
 }
