a1e483e86c543a1c1064cad1d4982a0ea5852510
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-05 10:08:12 +0200

    création de TrainMatinal (sans effet passif)

--- src/main/java/fr/umontpellier/iut/trains/cartes/TrainMatinal.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TrainMatinal.java
@@ -1,7 +1,16 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class TrainMatinal extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class TrainMatinal extends CarteRouge {
     public TrainMatinal() {
-        super("Train matinal");
+        super("Train matinal",5,2);
+        addCategorie(CategoriesCarte.BLEU);
+    }
+
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        //effet passif
     }
 }
