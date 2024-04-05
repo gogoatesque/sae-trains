f7a6979ab70324b1f907411054ae8b6b5d72d899
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-05 09:03:26 +0200

    correction BureauDuChefDeGare

--- src/main/java/fr/umontpellier/iut/trains/cartes/BureauDuChefDeGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/BureauDuChefDeGare.java
@@ -1,7 +1,22 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class BureauDuChefDeGare extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BureauDuChefDeGare extends CarteRouge {
     public BureauDuChefDeGare() {
-        super("Bureau du chef de gare");
+        super("Bureau du chef de gare", 4, 0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> choix = new ArrayList<>();
+        for (Carte carte : joueur.getMain()) {
+            if (carte.getCategorie().contains(CategoriesCarte.ROUGE)) choix.add(carte.getNom());
+        }
+        Carte action = joueur.getMain().getCarte(joueur.choisir("Choisissez votre carte action", choix, null, false));
+        if (action != null) action.jouerCarte(joueur);
     }
 }
