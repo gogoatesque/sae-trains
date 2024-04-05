944ed7bab5be03849da34f099fd6d32e609cb8e5
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-05 10:06:49 +0200

    renommage de l'activation de la carte et création de la carte

--- src/main/java/fr/umontpellier/iut/trains/cartes/Tunnel.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Tunnel.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class Tunnel extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class Tunnel extends CarteVerte {
     public Tunnel() {
-        super("Tunnel");
+        super("Tunnel",5);
+    }
+
+    @Override
+    public void activerEffet(Joueur joueur) {
+        //effet passif
     }
 }
