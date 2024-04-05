33474f82dc441df18de5d3adca2a320e8ca10972
Corentin Cano <corentin.cano@etu.umontpellier.fr>
2024-04-05 11:22:16 +0200

    création viaduc

--- src/main/java/fr/umontpellier/iut/trains/cartes/Viaduc.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Viaduc.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Viaduc extends Carte {
     public Viaduc() {
-        super("Viaduc");
+        super("Viaduc",5);
+    }
+
+    @Override
+    public void jouerCarte(Joueur Joueur) {
+        //effet passif
     }
 }
