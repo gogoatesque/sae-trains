64b4e1e0825d71cb5757c609cecd2c783d63760a
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-05 09:45:15 +0200

    dernière modif

--- src/main/java/fr/umontpellier/iut/trains/cartes/CabineDuConducteur.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CabineDuConducteur.java
@@ -1,7 +1,29 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class CabineDuConducteur extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CabineDuConducteur extends CarteRouge {
     public CabineDuConducteur() {
-        super("Cabine du conducteur");
+        super("Cabine du conducteur", 2, 0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> listeChoix = new ArrayList<>();
+        int nbCarte = 0;
+        for (Carte carte : joueur.getMain()) {listeChoix.add(carte.getNom());}
+        String choix = joueur.choisir("Choisissez vos cartes à défausser", listeChoix, null, true);
+        while(!choix.equals("")) {
+            joueur.placerCarteDefausse(joueur.getMain().getCarte(choix));
+            listeChoix.remove(choix);
+            nbCarte++;
+            choix = joueur.choisir("Choisissez vos cartes à défausser", listeChoix, null, true);
+        }
+        for(Carte carte : joueur.piocher(nbCarte)){
+            joueur.ajouterCarteEnMain(carte);
+        }
     }
 }
