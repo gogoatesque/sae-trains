78c97710a387dba614c256ef5782826409ed275a
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-15 13:30:37 +0200

    Depot

--- src/main/java/fr/umontpellier/iut/trains/cartes/Depot.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Depot.java
@@ -1,7 +1,28 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class Depot extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Depot extends CarteRouge {
     public Depot() {
-        super("Dépôt");
+        super("Dépôt",2,1);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<Carte> listeCartePiochee = joueur.piocher(2);
+        List<String> listeChoix = new ArrayList<>();
+        for (Carte c : listeCartePiochee) joueur.ajouterCarteEnMain(c);
+        for(Carte c : joueur.getMain()) listeChoix.add(c.getNom());
+        String s = joueur.choisir("Choisissez la carte à défausser", listeChoix, null, false);
+        joueur.placerCarteDefausse(joueur.getMain().getCarte(s));
+        joueur.enleverCarteMain(joueur.getMain().getCarte(s));
+        if (!joueur.getMain().isEmpty()) {
+            s = joueur.choisir("Choisissez la carte à défausser", listeChoix, null, false);
+            joueur.placerCarteDefausse(joueur.getMain().getCarte(s));
+            joueur.enleverCarteMain(joueur.getMain().getCarte(s));
+        }
     }
 }
