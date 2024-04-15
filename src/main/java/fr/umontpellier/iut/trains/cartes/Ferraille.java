c9cdea3d9706c876d9f3ac68b36f99c0959e31ec
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-15 13:40:25 +0200

    Feraille

--- src/main/java/fr/umontpellier/iut/trains/cartes/Ferraille.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Ferraille.java
@@ -1,7 +1,12 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Ferraille extends Carte {
     public Ferraille() {
-        super("Ferraille");
+        super("Ferraille", 0);
     }
+
+    @Override
+    public void jouerCarte(Joueur Joueur) {}
 }
