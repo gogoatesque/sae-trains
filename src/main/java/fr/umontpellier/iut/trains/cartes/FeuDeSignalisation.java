79ab4deedb46d76e3e0f2e6bf14f5bb9398a2bf6
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-15 14:01:47 +0200

    FeuDeSignalisation

--- src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
@@ -1,7 +1,27 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class FeuDeSignalisation extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeuDeSignalisation extends CarteRouge {
     public FeuDeSignalisation() {
-        super("Feu de signalisation");
+        super("Feu de signalisation", 2,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        Carte c = joueur.piocher();
+        joueur.ajouterCarteEnMain(c);
+        c = joueur.piocher();
+        if (c != null) {
+            List<String> listeChoix = new ArrayList<>();
+            listeChoix.add("oui");
+            listeChoix.add("non");
+            String choix = joueur.choisir("Voulez-vous défausser la carte (oui) ou replacer au dessus de la pioche (non) ?", listeChoix, null, false);
+            if (choix.equals("oui")) joueur.placerCarteDefausse(c);
+            else if (choix.equals("non")) joueur.placerDansPioche(c);
+        }
     }
 }
