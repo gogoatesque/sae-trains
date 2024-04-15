c5ac0dc6f6965463748deb23b1c412327ff89207
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-15 13:19:19 +0200

    Decharge

--- src/main/java/fr/umontpellier/iut/trains/cartes/Decharge.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Decharge.java
@@ -1,7 +1,19 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class Decharge extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class Decharge extends CarteRouge {
     public Decharge() {
-        super("Décharge");
+        super("Décharge", 2, 0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        for (Carte c : joueur.getMain()) {
+            if(c.getNom() == "Ferraille") {
+                joueur.getJeu().ajouterDansLaReserve(c);
+                joueur.enleverCarteMain(c);
+            }
+        }
     }
 }
