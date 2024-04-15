b1726135c75c8699fcb9a27a50f0d7c3d9a67c4e
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-15 20:51:11 +0200

    début carte Gare

--- src/main/java/fr/umontpellier/iut/trains/cartes/Gare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Gare.java
@@ -1,7 +1,28 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+import fr.umontpellier.iut.trains.plateau.Tuile;
+import fr.umontpellier.iut.trains.plateau.TuileVille;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gare extends Carte {
     public Gare() {
-        super("Gare");
+        super("Gare", 3);
+    }
+
+    @Override
+    public void jouerCarte(Joueur joueur) {
+        if (joueur.getJeu().getNbJetonsGare() != 0) {
+            List<String> choix = new ArrayList<>();
+            for (int i=0 ; i<76 ; i++){choix.add("TUILE:" + i);}
+            Tuile tuile = joueur.getJeu().getTuiles().get(Integer.parseInt(joueur.choisir("Choisissez une case où placer un rail",choix,null,false).substring(6)));
+            if(tuile instanceof TuileVille) {
+                //constuire une gare (à voir plus tard)
+            }
+        }
+        Carte ferraille = joueur.getJeu().prendreDansLaReserve("Ferraille");
+        if (ferraille != null) joueur.addCarteRecue(ferraille);
     }
 }
