d61ab6bf14a5965768c61724a971c1d418a019f3
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-16 21:49:25 +0200

    implémentation carte

--- src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
@@ -1,7 +1,28 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class HorairesTemporaires extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HorairesTemporaires extends CarteRouge {
     public HorairesTemporaires() {
-        super("Horaires temporaires");
+        super("Horaires temporaires", 5 ,0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        int cpt = 0;
+        List<Carte> cartesPiochees = new ArrayList<>();
+        while(cpt != 2) {
+            Carte carte = joueur.piocher();
+            if (carte == null) break;
+            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) {
+                cpt++;
+                joueur.ajouterCarteEnMain(carte);
+            }
+            else cartesPiochees.add(carte);
+        }
+        for (Carte c : cartesPiochees) joueur.placerCarteDefausse(c);
     }
 }
