82cba5a77a34aa6e32245e721c424238aa241bb8
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-16 21:49:18 +0200

    implémentation carte

--- src/main/java/fr/umontpellier/iut/trains/cartes/HorairesEstivaux.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/HorairesEstivaux.java
@@ -1,7 +1,24 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class HorairesEstivaux extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HorairesEstivaux extends CarteRouge {
     public HorairesEstivaux() {
-        super("Horaires estivaux");
+        super("Horaires estivaux",3, 0);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        List<String> listeChoix = new ArrayList<>();
+        listeChoix.add("oui");
+        listeChoix.add("non");
+        String choix = joueur.choisir("Voulez vous écarter la carte et recevoir 3¥ ou ne rien faire ?", listeChoix, null, false);
+        if (choix.equals("oui")) {
+            joueur.addArgent(3);
+            joueur.ecarteCarte(this);
+        }
     }
 }
