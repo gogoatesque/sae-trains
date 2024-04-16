69020db378c677c3a7d77ef67947ef6fe81985b5
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-16 21:49:13 +0200

    implémentation carte

--- src/main/java/fr/umontpellier/iut/trains/cartes/GratteCiel.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/GratteCiel.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class GratteCiel extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class GratteCiel extends CarteOr {
     public GratteCiel() {
-        super("Gratte-Ciel");
+        super("Gratte-Ciel", 8,4);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        joueur.addPv(getPv());
     }
 }
