76cce37b2c2fb2a38a75e0f0b904b01ad396388e
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-18 10:22:18 +0200

    implémentation carte immeuble

--- src/main/java/fr/umontpellier/iut/trains/cartes/Immeuble.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Immeuble.java
@@ -1,7 +1,14 @@
 package fr.umontpellier.iut.trains.cartes;
 
-public class Immeuble extends Carte {
+import fr.umontpellier.iut.trains.Joueur;
+
+public class Immeuble extends CarteOr {
     public Immeuble() {
-        super("Immeuble");
+        super("Immeuble", 5, 2);
+    }
+
+    @Override
+    public void faireAction(Joueur joueur) {
+        joueur.addPv(getPv());
     }
 }
