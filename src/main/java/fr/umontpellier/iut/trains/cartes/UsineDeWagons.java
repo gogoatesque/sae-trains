78844cd01714fb1e125c5b4ab7c8201b05be1c38
Cano Corentin <corentin.cano@etu.umontpellier.fr>
2024-04-18 17:09:50 +0200

    complétion de UsineDeWagons

--- src/main/java/fr/umontpellier/iut/trains/cartes/UsineDeWagons.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/UsineDeWagons.java
@@ -12,14 +12,19 @@ public class UsineDeWagons extends CarteRouge {
 
     @Override
     public void faireAction(Joueur joueur) {
-        List<String> choix = new ArrayList<>();
+        List<String> choixMain = new ArrayList<>();
         for (Carte carte : joueur.getMain()) {
-            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) choix.add(carte.getNom());
+            if (carte.getCategorie().contains(CategoriesCarte.BLEU)) choixMain.add(carte.getNom());
         }
-        Carte train = joueur.getMain().getCarte(joueur.choisir("Choisissez une carte Train de votre main a écartée", choix, null, false));
-        if(train != null) {
-            joueur.ecarteCarte(train);
-            choix = joueur.getJeu().getReserve().keySet().stream().filter(nomCarte -> joueur.getJeu().getReserve().get(nomCarte).getCarte(nomCarte).getCout() <= train.getValeur()+3).map(nomCarte -> "ACHAT:"+nomCarte).toList();
+        Carte trainMain = joueur.getMain().getCarte(joueur.choisir("Choisissez une carte Train de votre main a écartée", choixMain, null, false));
+        if(trainMain != null) {
+            List<String> choixReserve;
+            joueur.ecarteCarte(trainMain);
+            joueur.getMain().remove(trainMain);
+
+            choixReserve = joueur.getJeu().getReserve().keySet().stream().filter(nomCarte -> !joueur.getJeu().getReserve().get(nomCarte).isEmpty() && joueur.getJeu().getReserve().get(nomCarte).get(0).getCategorie().contains(CategoriesCarte.BLEU) && joueur.getJeu().getReserve().get(nomCarte).get(0).getCout() <= trainMain.getCout()+3).map(nomCarte -> "ACHAT:"+nomCarte).toList();
+            Carte trainReserve = joueur.getJeu().prendreDansLaReserve(joueur.choisir("Choisissez une carte train de la reserve coutant maximum 3 de plus", choixReserve, null, false).substring (6));
+            joueur.ajouterCarteEnMain(trainReserve);
         }
     }
 }
