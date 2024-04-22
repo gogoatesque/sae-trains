7a673fa504554460a3926e44a76813f95da67696
Goharik <goharik.gyurjyan@etu.umontpellier.fr>
2024-04-22 16:01:44 +0200

    ajout boutons

--- src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
 import fr.umontpellier.iut.trains.Joueur;
 
 import java.util.ArrayList;
@@ -19,7 +20,10 @@ public class FeuDeSignalisation extends CarteRouge {
             List<String> listeChoix = new ArrayList<>();
             listeChoix.add("oui");
             listeChoix.add("non");
-            String choix = joueur.choisir("Voulez-vous défausser la carte (oui) ou replacer au dessus de la pioche (non) ?", listeChoix, null, false);
+            List<Bouton> boutons = new ArrayList<>();
+            boutons.add(new Bouton("Défausser", "oui"));
+            boutons.add(new Bouton("Replacer sur la pioche", "non"));
+            String choix = joueur.choisir("Voulez-vous défausser la carte (oui) ou replacer au dessus de la pioche (non) ?", listeChoix, boutons, false);
             if (choix.equals("oui")) joueur.placerCarteDefausse(c);
             else if (choix.equals("non")) joueur.placerDansPioche(c);
         }
